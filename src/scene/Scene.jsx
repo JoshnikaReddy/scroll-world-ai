0e4101e30bdee5de4afbb6951725104fc2943ad6
JoshnikaReddy <joshnikareddy.deverapally24@st.niituniversity.in>
2026-08-05 00:18:10 +0530

    Added ground and lighting components

--- src/scene/Scene.jsx
+++ src/scene/Scene.jsx
@@ -1,23 +1,23 @@
 import { Canvas } from "@react-three/fiber";
+import Lights from "../components/lighting/Lights";
+import Ground from "../components/world/Ground";
 
 export default function Scene() {
   return (
     <Canvas
+      shadows
       camera={{
-        position: [0, 2, 8],
+        position: [0, 3, 8],
         fov: 45,
       }}
     >
       <color attach="background" args={["#050505"]} />
 
-      <ambientLight intensity={2} />
+      <Lights />
 
-      <directionalLight
-        position={[5, 5, 5]}
-        intensity={2}
-      />
+      <Ground />
 
-      <mesh>
+      <mesh position={[0, 1, 0]} castShadow>
         <boxGeometry args={[2, 2, 2]} />
         <meshStandardMaterial color="#ff7a00" />
       </mesh>
